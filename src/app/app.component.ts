a91ed7ab5aef3227cc0c10efd3815a65267f61d0
AlbertBabaiani <BabaianiAlbert@gmail.com>
2024-02-28 22:28:42 +0400

    Final release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { ChangePageService } from './Services/change-page.service';
-import { Pages } from './nav-bar/nav-bar.component';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'UserList';
-
-  current_page: Pages = 'home'
-
-  pages_service = inject(ChangePageService)
-
-  ngOnInit(){
-    this.pages_service.current_page.subscribe({
-      next: (new_page: Pages) =>{
-        this.current_page = new_page
-      }
-    })
-  }
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,8 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { SubscribtionService } from '../Services/subscribtion.service';
-import { ChangePageService } from '../Services/change-page.service';
-
-export type Pages = 'home' | 'admin'
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,16 +7,7 @@ export type Pages = 'home' | 'admin'
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
-  current_page: Pages = 'home'
-
   subscribe_service = inject(SubscribtionService)
-  pages_service = inject(ChangePageService)
-
-  changePage(new_page: Pages): void{
-    this.current_page = new_page
-    this.pages_service.changePage(new_page)
-  }
-
 
   registerSubscribtion(): void{
     this.subscribe_service.onSubscribe()
